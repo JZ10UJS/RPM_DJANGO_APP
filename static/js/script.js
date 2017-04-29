2bd4aaaea5049b2e46e2662596275fa17916dc8d
zhangjie <zhangj_10ujs@outlook.com>
2017-04-29 21:52:54 +0800

    'init';

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+// $(document).ready(function(){
+//     console.log('ready info.');
+//     $('a').on('click', function(event){
+//         console.log('You\'r click a link');
+//         event.preventDefault();
+//         return false;
+//     })
+// });
