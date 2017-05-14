f7d72e8d44c27f4dd990073d5f00c51943cc3b07
zhangjie <zhangj_10ujs@outlook.com>
2017-05-14 23:01:55 +0800

    fix mistake

--- make_rpm.sh
+++ make_rpm.sh
@@ -2,7 +2,7 @@ $cwd=`pwd`
 mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
 
 python setup.py sdist
-cp sdist/django-scan-0.1.tar.gz ~/rpmbuild/SOURCES/
+cp dist/django-scan-0.1.tar.gz ~/rpmbuild/SOURCES/
 cp django-scan.spec ~/rpmbuild/SPECS/
 
 rpmbuild -bb ~/rpmbuild/SPECS/django-scan.spec
